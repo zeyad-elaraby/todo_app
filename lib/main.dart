2a4d0e47c84ed987233bc0c6eea13c4ea90cf65f
ZEYAD <zeyadelaraby1@gmail.com>
2024-08-20 22:30:02 +0300

    finish change app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/my_theme_data.dart';
 import 'package:todo_app/providers/my_provider.dart';
 
+import 'firebase_options.dart';
 import 'home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
       create: (context) => MyProvider(),
       child: EasyLocalization(
